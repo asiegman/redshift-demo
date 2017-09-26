ef1c67dd5ed3e5740ebd2eda8de1d891107a40f5
Alex Siegman <asiegman@gmail.com>
2017-09-26 12:38:57 +0900

    Quick Redshift CF/TF Demo

---
+++ deploy.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
+
+variable "ChargeCode" {
+  type = "string"
+  description = "Billing Charge Code"
+  default = "dev"
+}
+
+resource "aws_cloudformation_stack" "redshift" {
+  name = "dev-redshift"
+
+  parameters = {
+    ClusterName = "devredshift"
+    ChargeCode = "${var.ChargeCode}"
+    ClusterType = "multi-node"
+    NumberOfNodes = 2
+    NodeType = "ds2.xlarge"
+    MasterUserPassword = "TotallyInsecurePassword123"
+  }
+
+  template_body = "${file("${path.module}/cf-redshift.yaml")}"
+  timeout_in_minutes = 30
+  capabilities = ["CAPABILITY_NAMED_IAM"]
+  on_failure = "DO_NOTHING"
+
+  tags {
+    ChargeCode = "${var.ChargeCode}"
+  }
+}
